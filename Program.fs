7375344deb5f527537af80cf43fc5a6beb5abf2b
jmaz <jmaz@DESKTOP-8O67RIL>
2023-05-07 20:06:07 -0500

    Working on starting timers and stuff

--- Program.fs
+++ Program.fs
@@ -1,4 +1,29 @@
-﻿[<EntryPoint>]
-let main args = 
-  printfn "Hello from F#"
-  0
+﻿let rec readInput (agent: MailboxProcessor<string>) =
+    let input = System.Console.ReadLine()
+    agent.Post input
+    readInput agent
+
+let printer (inbox: MailboxProcessor<string>) =
+    let timer = System.Diagnostics.Stopwatch()
+
+    let rec messageLoop (timer: System.Diagnostics.Stopwatch) =
+        async {
+            let! msg = inbox.TryReceive(1000)
+
+            match msg with
+            | Some _ -> timer.Start()
+            | None -> printfn "%s" (timer.Elapsed.ToString())
+
+            return! messageLoop timer
+        }
+
+    messageLoop timer
+
+[<EntryPoint>]
+let main args =
+    printfn "Please select things"
+
+    let printerAgent = MailboxProcessor.Start(printer)
+
+    readInput printerAgent
+    0
