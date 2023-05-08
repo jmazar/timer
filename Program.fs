27d6bf74e613e354b80e988350303cf9c33bac33
jmaz <jmaz@DESKTOP-8O67RIL>
2023-05-07 20:38:30 -0500

    Kinda getting things to timer

--- Program.fs
+++ Program.fs
@@ -1,23 +1,57 @@
-﻿let rec readInput (agent: MailboxProcessor<string>) =
+﻿open System.Collections.Generic
+
+type MyTimer =
+    { Name: string
+      Timer: System.Diagnostics.Stopwatch }
+
+let rec readInput (agent: MailboxProcessor<string>) =
     let input = System.Console.ReadLine()
     agent.Post input
     readInput agent
 
-let printer (inbox: MailboxProcessor<string>) =
-    let timer = System.Diagnostics.Stopwatch()
 
-    let rec messageLoop (timer: System.Diagnostics.Stopwatch) =
+let printOutput (timer: MyTimer option) =
+    match timer with
+    | Some timer ->
+        System.Console.Clear()
+        printfn "%s: %s" timer.Name (timer.Timer.Elapsed.ToString())
+    | None -> ()
+
+let printer (inbox: MailboxProcessor<string>) =
+    let timers = Dictionary<string, MyTimer>()
+
+    let rec messageLoop (timers: Dictionary<string, MyTimer>) (currentTimer: MyTimer option) =
         async {
             let! msg = inbox.TryReceive(1000)
 
-            match msg with
-            | Some _ -> timer.Start()
-            | None -> printfn "%s" (timer.Elapsed.ToString())
+            let currentTimer =
+                match msg with
+                | Some string ->
+                    match currentTimer with
+                    | Some timer -> timer.Timer.Stop()
+                    | None -> ()
 
-            return! messageLoop timer
+                    let currentTimer =
+                        match timers.ContainsKey(string) with
+                        | true -> timers[string]
+                        | false ->
+                            let newtimer =
+                                { Name = string
+                                  Timer = System.Diagnostics.Stopwatch() }
+
+                            timers.Add(string, newtimer)
+                            newtimer
+
+                    currentTimer.Timer.Start()
+                    Some currentTimer
+                | None ->
+                    printOutput currentTimer
+                    currentTimer
+
+            return! messageLoop timers currentTimer
         }
 
-    messageLoop timer
+    messageLoop timers None
 
 [<EntryPoint>]
 let main args =
